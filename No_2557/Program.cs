5801c39ee60e6f10272862e0c5c6570c64fefede
MetroDefro <wadmom0913@gmail.com>
2023-05-30 17:48:54 +0900

    백준 2557번 문제 Hello World https://www.acmicpc.net/problem/2557

---
+++ No_2557/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace No_2557
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
