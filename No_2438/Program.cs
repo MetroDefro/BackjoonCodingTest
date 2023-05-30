898cc98e86eca7e5092a096f61b9bff1efbee8b4
MetroDefro <wadmom0913@gmail.com>
2023-05-30 17:32:43 +0900

    백준 2438번 문제 별 찍기 - 1 https://www.acmicpc.net/problem/2438

---
+++ No_2438/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace No_2438
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int num = int.Parse(input);
+
+            for(int i = 1; i <= num; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
