3ad598821e4a5e3248ab389fe48c1002c4772bf0
MetroDefro <wadmom0913@gmail.com>
2023-05-30 17:15:55 +0900

    백준 1008번 문제 A/B https://www.acmicpc.net/problem/1008

---
+++ No_1008/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace No_1008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] result = input.Split(' ');
+            double a = double.Parse(result[0]);
+            double b = double.Parse(result[1]);
+            Console.WriteLine(a / b);
+        }
+    }
+}
