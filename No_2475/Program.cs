aac4c00275053b202f3388c7b03988dee8199b44
MetroDefro <wadmom0913@gmail.com>
2023-05-30 17:46:50 +0900

    백준 2475번 문제 검증수 https://www.acmicpc.net/problem/2475

---
+++ No_2475/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace No_2475
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                sum += (int)Math.Pow(int.Parse(input.Split()[i]), 2);
+            }
+
+            Console.WriteLine(sum % 10);
+        }
+    }
+}
