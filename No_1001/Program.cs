6369ad34431719e1a59957248a9ef6df9516ed47
MetroDefro <wadmom0913@gmail.com>
2023-05-30 17:09:22 +0900

    백준 1001번 문제 A-B https://www.acmicpc.net/problem/1001

---
+++ No_1001/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace No_1001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] result = input.Split(' ');
+            int a = int.Parse(result[0]);
+            int b = int.Parse(result[1]);
+            Console.WriteLine(a - b);
+        }
+    }
+}
