72afe72479d3fb9cf3a41fd9a8e21044c480064e
MetroDefro <wadmom0913@gmail.com>
2023-05-30 17:28:19 +0900

    백준 1330번 문제 두 수 비교하기 https://www.acmicpc.net/problem/1330

---
+++ No_1330/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace No_1330
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] result = input.Split(' ');
+            int a = int.Parse(result[0]);
+            int b = int.Parse(result[1]);
+            if(a == b)
+                Console.WriteLine("==");
+            else if(a < b)
+                Console.WriteLine("<");
+            else
+                Console.WriteLine(">");
+        }
+    }
+}
