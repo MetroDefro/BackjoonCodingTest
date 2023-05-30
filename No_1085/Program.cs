a45ded90f46b4091488c7881f12ba46f243ef5cf
MetroDefro <wadmom0913@gmail.com>
2023-05-30 18:21:46 +0900

    백준 1085번 문제 직사각형에서 탈출 https://www.acmicpc.net/problem/1085

---
+++ No_1085/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace No_1085
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int x = int.Parse(input.Split()[0]);
+            int y = int.Parse(input.Split()[1]);
+            int w = int.Parse(input.Split()[2]);
+            int h = int.Parse(input.Split()[3]);
+            
+            int min = x < y ? x : y;
+            if(w - x < min)
+                min = w - x;
+            if(h - y < min)
+                min = h - y;
+
+            Console.WriteLine(min);
+        }
+    }
+}
