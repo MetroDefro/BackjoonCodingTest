8e32ec0583a8c2be492f0d680240e982eca6e792
MetroDefro <wadmom0913@gmail.com>
2023-06-16 13:01:18 +0900

    18111

---
+++ BackjoonCodingTest/No_18111.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackjoonCodingTest
+{
+    public class No_18111
+    {
+        public No_18111()
+        {
+            using var reader = new System.IO.StreamReader(Console.OpenStandardInput());
+            using var print = new System.IO.StreamWriter(Console.OpenStandardOutput());
+
+            string[] inputs = reader.ReadLine().Split();
+
+            int N = int.Parse(inputs[0]);
+            int M = int.Parse(inputs[1]);
+            int B = int.Parse(inputs[2]);
+
+            int[] heights = new int[256 + 1];
+            int min = 256;
+            int max = 0;
+            for (int i = 0; i < N; i++)
+            {
+                inputs = reader.ReadLine().Split();
+                for (int j = 0; j < M; j++)
+                {
+                    int temp = int.Parse(inputs[j]);
+
+                    if (min > temp)
+                        min = temp;
+
+                    if (max < temp)
+                        max = temp;
+
+                    heights[temp]++;
+                }
+            }
+
+            int minCount = 128000000;
+            int maxIndex = 0;
+            for (int i = min; i <= max; i++)
+            {
+                int upCount = 0;
+                int downCount = 0;
+                int inven = B;
+                for (int j = max; j >= min; j--)
+                {
+                    if (i > j)
+                    {
+                        upCount += (heights[j] * (i - j));
+                        if (upCount > inven)
+                        {
+                            upCount = -1;
+                            break;
+                        }
+
+                    }
+                    else if (i < j)
+                    {
+                        int temp = heights[j] * (j - i);
+                        inven += temp;
+                        downCount += (temp * 2);
+                    }
+
+                }
+
+                if (upCount == -1)
+                    continue;
+
+                if (minCount >= (upCount + downCount))
+                {
+                    minCount = upCount + downCount;
+                    maxIndex = i;
+                }
+            }
+
+            print.WriteLine(minCount + " " + maxIndex);
+        }
+    }
+}
--- BackjoonCodingTest/Program.cs
+++ BackjoonCodingTest/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BackjoonCodingTest
 {
@@ -11,9 +9,24 @@ namespace BackjoonCodingTest
             using var reader = new System.IO.StreamReader(Console.OpenStandardInput());
             using var print = new System.IO.StreamWriter(Console.OpenStandardOutput());
 
-            int n = int.Parse(reader.ReadLine());
+            string[] inputs = reader.ReadLine().Split();
+
+            int T = int.Parse(inputs[0]);
+
+            inputs = reader.ReadLine().Split();
+
+            int M = int.Parse(inputs[0]);
+            int N = int.Parse(inputs[1]);
+            int K = int.Parse(inputs[2]);
+
+            bool[,] field = new bool[M, N]; 
+            for (int i = 0; i < N; i++)
+            {
+                inputs = reader.ReadLine().Split();
+                field[int.Parse(inputs[0]), int.Parse(inputs[1])] = true;
 
 
+            }
         }
     }
 }
